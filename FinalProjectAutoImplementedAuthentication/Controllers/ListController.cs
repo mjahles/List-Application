84b329ded3aa4dffc31ca67cffd2005cc1469def
Ahles <ahlmatj@dunwoody.edu>
2021-03-22 13:28:21 -0500

    Created ListController.cs.  Created CreateList ActionResult that is currently just a placeholder.  Created a view for the IndexList ActionResult (This Renders the List CRUD operations).  Added a link to the IndexList view from the _Layout.cshtml file.  Set authorization to be required for accessing List CRUD operations.

---
+++ FinalProjectAutoImplementedAuthentication/Controllers/ListController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProjectAutoImplementedAuthentication.Controllers
+{
+    public class ListController : Controller
+    {
+        // GET: List
+        [Authorize]
+        public ActionResult IndexList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateList()
+        {
+            return View();
+        }
+    }
+}
